1c25022a38aa1a02b600bc079c3a67d457ec4ed7
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-26 20:36:38 +1100

    Add class Program

--- new_modifier/Program.cs
+++ new_modifier/Program.cs
@@ -14,11 +14,26 @@ namespace new_modifier
         }
     }
 
-    class TEST1 : TEST
+    class TEST1 : Test
     {
         public new void Func()
         {
             Console.WriteLine("TEST1.func");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Test ref1 = new Test();
+            Test ref2 = new TEST1();
+            TEST1 ref3 = new TEST1();
+
+            ref1.Func();
+            ref2.Func();
+            ref3.Func();
+            Console.Read();
         }
     }
 }
