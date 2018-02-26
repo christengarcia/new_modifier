b54642cd9479e4cbdfb19514bb1172013092b7b0
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-26 20:31:33 +1100

    Initial commit

---
+++ new_modifier/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ *  C# Program to Demonstrate New modifier
+ */
+
+using System;
+
+namespace new_modifier
+{
+    class Test
+    {
+        public void Func()
+        {
+            Console.WriteLine("TEST.func");
+        }
+    }
+}
